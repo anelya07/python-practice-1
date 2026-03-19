db0db6a37b5452d9b8da49fe63c53d3a18fa0b32
anelya07 <253652@astanait.edu.kz>
2026-03-19 16:54:00 +0500

    task 1a

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, Python!")
+#TASK 1 - A
+
+# a)
+customer = input("Enter customer name: ")
+product = input("Enter product name: ")
+price = float(input("Enter price per unit (KZT): "))
+quantity = int(input("Enter quantity: "))
+
+
+
