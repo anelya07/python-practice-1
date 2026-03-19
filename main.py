55e17282a7801e4e9a81d8245828513b27808240
anelya07 <253652@astanait.edu.kz>
2026-03-19 17:41:56 +0500

    task 1c

--- main.py
+++ main.py
@@ -6,18 +6,30 @@ product = input("Enter product name: ")
 price = float(input("Enter price per unit (KZT): "))
 quantity = int(input("Enter quantity: "))
 
-# b)
+# (b) and (c)
 
+print("=" * 30)
+print("         SHOP RECEIPT")
+print("=" * 30)
+
+print(f"Customer: {customer}")
+print(f"Product: {product}")
+print(f"Price: {price}" + " KZT")
+print(f"Quantity: {quantity}")
+
+print("-" * 30)
 subtotal = price * quantity
 
 if subtotal>5000:
-    print("Subtotal: ", subtotal)
+    print(f"Subtotal: {subtotal}" + " KZT")
     discount = subtotal * 0.1
     total = subtotal - discount
-    print("Discount: ", discount)
-    print("Total: ", total)
+    print(f"Discount: {discount}" + " KZT")
+    print(f"Total: {total}" + " KZT")
 else:
-    print("Total: ", subtotal)
+    print(f"Total: {subtotal}" + " KZT")
+
+print("=" * 30)
 
 
 
